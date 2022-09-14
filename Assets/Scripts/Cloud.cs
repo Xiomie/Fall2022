dc1a2a949822f92e315c1a76b14d145c1ccad3dc
Xiomie <mikegerxiong37@gmail.com>
2022-09-14 17:26:43 -0400

    Summary

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,26 @@
+//library
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//main class of this script
+public class Cloud : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // public float speed = 0.1f;
+        //Debug.Log("hello world");
+        //Debug.Log(this.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       //Debug.Log("Frame");
+        //this.transform.position = new Vector3(this.transform.position.x, 
+             //this.transform.position.y, this.transform.position.z + (speed * 0.1f * Time.deltaTime));
+        //Debug.Log(-.1f * Time.deltaTime);
+
+    }
+}
